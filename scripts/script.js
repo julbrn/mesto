40d57b7860d781a32d9f61d8ee7ed88f46a09747
Yulia Burenko <juliaburenko@yandex.ru>
2022-05-24 18:01:08 +0300

    feat: Add an  image array & toggle likes

--- scripts/script.js
+++ scripts/script.js
@@ -28,4 +28,55 @@ editButton.addEventListener('click', openPopup);
 closeButton.addEventListener('click', closePopup);
 editForm.addEventListener('submit', formSubmitHandler);
 
+//Архив карточек
+const initialCards = [
+  {
+    name: 'Карачаевск',
+    link: './images/elements/Karachaevsk.jpg'
+  },
+  {
+    name: 'Бермамыт',
+    link: './images/elements/Bermamyt_Plateau1.jpg'
+  },
+  {
+    name: 'Гора Эльбрус',
+    link: './images/elements/Elbrus.jpg'
+  },
+  {
+    name: 'Гора Машук',
+    link: './images/elements/Mount_Mashuk1.jpg'
+  },
+  {
+    name: 'Ущелье Птыш',
+    link: './images/elements/Chuchkhur_waterfall1.jpg'
+  },
+  {
+    name: 'Домбай',
+    link: './images/elements/Dombai.jpg'
+  }
+];
+
+const likeButtonHandler = (evt) => {
+  evt.target.classList.toggle('card__like-button_active');
+}
+
+const cards = document.querySelector('.elements__cards');
+
+initialCards.forEach(({link, name}) => {
+  const template = document.querySelector('.card-template').content.querySelector('.card').cloneNode(true);
+  const cardTitle = template.querySelector('.card__title');
+  const cardPhoto = template.querySelector('.card__photo');
+  const deleteButton = template.querySelector('.card__remove-button');
+  const likeButton = template.querySelector('.card__like-button');
+  likeButton.addEventListener('click', likeButtonHandler);
+  cardPhoto.src = link;
+  cardPhoto.alt = name;
+  cards.append(template);
+});
+
+
+
+
+
+
 
